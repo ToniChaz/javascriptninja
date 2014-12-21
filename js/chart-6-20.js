dcabf47dc429b912ee802bff7c517891593ecde4
Toni Chaz <toni.chaz@hotmail.com>
2014-12-21 18:40:53 +0100

    add 6.20 exercise

---
+++ js/chart-6-20.js
@@ -0,0 +1,71 @@
+var JSClass = function(){
+	
+	(function(){
+	
+	var initializing = false, 
+		superPattern = /xyz/.test(function(){ xyz; }) ? /\b_super\b/ : /.*/;
+		
+		Object.subClass = function(properties) {
+			var _super = this.prototype;
+			initializing = true;
+			var proto = new this();
+			initializing = false;
+			
+			for (var name in properties){
+				proto[name] = typeof properties[name] == "function" 
+				&& typeof _super[name] == "function" 
+				&& superPattern.test(properties[name]) ?
+				
+				(function(name, fn){
+					return function(){
+						var tmp = this._super;
+						this._super = _super[name];
+						var ret = fn.apply(this, arguments);
+						this._super = tmp;
+						return ret;
+					};
+				})(name, properties[name]) : 
+				properties[name];				
+			}
+			function Class(){
+				if(!initializing && this.init)
+					this.init.apply(this, arguments);
+			}
+			
+			Class.prototype = proto;
+			Class.constructor = Class;
+			Class.subClass = arguments.callee;
+			return Class;
+		};
+	})();
+	
+	var Person = Object.subClass({
+		init: function(isDancing){
+			this.dancing = isDancing;
+		},
+		dance: function(){
+			return this.dancing;
+		}
+	});
+	
+	var Ninja = Person.subClass({
+		init: function(){
+			this._super(false);
+		},
+		dance: function(){
+			this._super();
+		},
+		swingSword: function(){
+			return true;
+		}	
+	});
+	
+	var person = new Person(true);
+	assert(person.dance(), "The person is dancing.");
+	
+	var ninja = new Ninja();
+	assert(ninja.swingSword(), "The sword is swinging.");	
+	assert(!ninja.dance(), "The ninja is not dancing.");
+	assert(person instanceof Person, "The person is a Person");
+	assert(ninja instanceof Ninja && ninja instanceof Person, "The ninja is Ninja and Person");
+};
